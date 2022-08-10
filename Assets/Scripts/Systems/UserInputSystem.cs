abe303a27dd7f58384bcdc7a227274813dafb38e
Romanov Nikita <91887896+BIGNIKi@users.noreply.github.com>
2022-08-10 22:47:49 +0700

    2.4.0

---
+++ Assets/Scripts/Systems/UserInputSystem.cs
@@ -0,0 +1,14 @@
+using Unity.Entities;
+
+namespace Systems
+{
+    public class UserInputSystem : ComponentSystem
+    {
+        private EntityQuery _movementQuery;
+        
+        protected override void OnUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
