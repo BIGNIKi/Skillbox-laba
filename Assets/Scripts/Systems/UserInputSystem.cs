acbaeec6bb15a6460f604cabf4faa2db5c380721
Romanov Nikita <91887896+BIGNIKi@users.noreply.github.com>
2022-08-10 23:02:51 +0700

    2.4.1

--- Assets/Scripts/Systems/UserInputSystem.cs
+++ Assets/Scripts/Systems/UserInputSystem.cs
@@ -1,14 +1,25 @@
 using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine.InputSystem;
 
-namespace Systems
-{
-    public class UserInputSystem : ComponentSystem
-    {
-        private EntityQuery _movementQuery;
-        
-        protected override void OnUpdate()
-        {
-            throw new System.NotImplementedException();
-        }
-    }
+namespace Systems {
+	public class UserInputSystem : ComponentSystem {
+		private EntityQuery _inputQuery; // запрос на движение
+		private InputAction _moveAction; // это наш ввод
+		private float2      _moveInput;
+
+		// Запускается при создании системы (что-то типа awake)
+		protected override void OnCreate() {
+			// кешируем запрос, который будет искать все Entity с компонентом UserInputData
+			_inputQuery = GetEntityQuery(ComponentType.ReadOnly<UserInputData>());
+		}
+	// TODO: остановился на 4:50
+		protected override void OnStartRunning() {
+			//_moveAction = new InputAction()
+		}
+
+		protected override void OnUpdate() {
+			throw new System.NotImplementedException();
+		}
+	}
 }
